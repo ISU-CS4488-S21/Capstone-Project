b2ca179655b4fe7a2e055e96b6f5217228252993
Andres <sewellandres@gmail.com>
2021-02-03 10:56:31 -0700

    Header file stubs, adjusted gitignore

---
+++ unit-commitment/parser.cpp
@@ -0,0 +1,8 @@
+#include "parser.h"
+
+// Just an example for how to implement class member functions
+// The definition exists under parser.h
+void Parser::loadData(std::string filepath) {
+    // Implementation code goes here
+}
+
