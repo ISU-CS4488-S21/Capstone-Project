c012f80fc00787bc61eed02579e6f7743befc135
Marcus <mbgoec2000@gmail.com>
2021-02-19 15:30:51 -0700

    Added class level variables, getters, and enum type to generator class

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,4 +1,19 @@
+#include "generator.h"
 
-    double sum(int x, int y){
+Generator::Generator(double fuelCost, double startUpCost, GeneratorType type) {
+    this->fuelCost = fuelCost;
+    this->startUpCost = startUpCost;
+    this->type = type;
+}
+
+double Generator::getFuelCost() {
+    return fuelCost;
+}
+
+double Generator::getStartUpCost() {
+    return startUpCost;
+}
+
+double Generator::sum(int x, int y){
     return x+y;
 }
--- src/generator.h
+++ src/generator.h
@@ -1,4 +1,26 @@
 #ifndef UNIT_COMMITMENT_GENERATOR_H
 #define UNIT_COMMITMENT_GENERATOR_H
-double sum(int x, int y);
+
+class Generator {
+
+    private:
+        double fuelCost{};
+        double startUpCost{};
+        enum GeneratorType {
+            Coal,
+            Nuclear,
+            Hydro,
+            Wind,
+            Solar,
+        };
+
+        GeneratorType type;
+
+
+    public:
+        double sum(int x, int y);
+        Generator(double fuelCost, double startUpCost, GeneratorType type);
+        double getFuelCost();
+        double getStartUpCost();
+};
 #endif //UNIT_COMMITMENT_GENERATOR_H
