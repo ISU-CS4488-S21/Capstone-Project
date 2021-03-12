152f93426c4980b1379461d53d8d2f5320ccb822
Marcus <mbgoec2000@gmail.com>
2021-03-11 19:49:57 -0700

    Modify generator class

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,13 +1,63 @@
 #include "generator.h"
+#include<cmath>
 
-Generator::Generator(GeneratorType gt, double fCost, double stCost, double shCost, double bRate, int pOut) {
+Generator::Generator(GeneratorType gt, int powerState) {
     type = gt;
-    fuelCost = fCost;
-    startUpCost = stCost;
-    shutDownCost = shCost;
-    burnRate = bRate;
-    powerOut = pOut;
-    isOn = false;
+    isOn = powerState;
+
+    // economicDispatchCost currently uses maxPowerOut
+    if (gt == CoalFiredSteam) {
+        fuelCost = 1;
+        startUpCost = 0;
+        shutDownCost = 0;
+        maxPowerOut = 0;
+        minPowerOut = 0;
+        A = 0;
+        B = 0;
+        C = 0;
+        economicDispatchCost = A + B*maxPowerOut + C*pow(maxPowerOut, 2);
+    } else if (gt == OilFiredSteam){
+        fuelCost = 1;
+        startUpCost = 0;
+        shutDownCost = 0;
+        maxPowerOut = 0;
+        minPowerOut = 0;
+        A = 0;
+        B = 0;
+        C = 0;
+        economicDispatchCost = A + B*maxPowerOut + C*pow(maxPowerOut, 2);
+    } else if (gt == SmallSub) {
+        fuelCost = 1;
+        startUpCost = 0;
+        shutDownCost = 0;
+        maxPowerOut = 0;
+        minPowerOut = 0;
+        A = 0;
+        B = 0;
+        C = 0;
+        economicDispatchCost = 0;
+    } else if (gt == LargeSub) {
+        fuelCost = 1;
+        startUpCost = 0;
+        shutDownCost = 0;
+        maxPowerOut = 0;
+        minPowerOut = 0;
+        A = 0;
+        B = 0;
+        C = 0;
+        economicDispatchCost = A + B*maxPowerOut + C*pow(maxPowerOut, 2);
+    } else if (gt == OtherSteam) {
+        fuelCost = 1;
+        startUpCost = 0;
+        shutDownCost = 0;
+        maxPowerOut = 0;
+        minPowerOut = 0;
+        A = 0;
+        B = 0;
+        C = 0;
+        economicDispatchCost = A + B*maxPowerOut + C*pow(maxPowerOut, 2);
+    }
+
 }
 
 double Generator::getFuelCost() {
--- src/generator.h
+++ src/generator.h
@@ -3,34 +3,34 @@
 
 class Generator {
 
-    private:
-        double fuelCost, startUpCost, shutDownCost, burnRate;
-        int powerOut;
-        bool isOn;
-        enum GeneratorType {
-            Coal,
-            Nuclear,
-            Hydro,
-            Wind,
-            Solar,
-        };
+private:
+    double fuelCost, startUpCost, shutDownCost, economicDispatchCost, A, B, C;
+    int maxPowerOut, minPowerOut;
+    int isOn;
+    enum GeneratorType {
+        CoalFiredSteam,
+        OilFiredSteam,
+        SmallSub,
+        LargeSub,
+        OtherSteam,
+    };
 
-        GeneratorType type;
+    GeneratorType type;
 
 
-    public:
-        double sum(int x, int y);
-        Generator(GeneratorType gt, double fCost, double stCost, double shCost, double bRate, int pOut);
+public:
+    double sum(int x, int y);
+    Generator(GeneratorType gt);
 
-        double getFuelCost();
-        double getStartUpCost();
-        double getShutDownCost();
-        double getBurnRate();
-        int getPowerOut();
-        bool getIsOn();
-        void turnOn();
-        void turnOff();
-        GeneratorType getGeneratorType();
+    double getFuelCost();
+    double getStartUpCost();
+    double getShutDownCost();
+    double getBurnRate();
+    int getPowerOut();
+    int getIsOn();
+    void turnOn();
+    void turnOff();
+    GeneratorType getGeneratorType();
 
 };
 #endif //UNIT_COMMITMENT_GENERATOR_H
