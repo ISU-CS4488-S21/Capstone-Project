eea34709042f42d147aba9444b5a2e746232867d
Andres Sewell <sewellandres@gmail.com>
2021-03-14 16:27:31 -0600

    Finished fleshing out the combination algorithm/concept

--- src/combination.cpp
+++ src/combination.cpp
@@ -1,32 +1,41 @@
-// Not quite working
-#include <vector>
-#include <iterator>
+// This file only exists as a space to get the concept working, and should be deleted later with it's code being moved
+// to wherever it is needed.
 #include <iostream>
+#include <cmath>
 
-const int size = 3;
+// Algorithm citation: https://stackoverflow.com/questions/8374079/fastest-way-to-generate-all-binary-strings-of-size-n-into-a-boolean-array
+void comboExample() {
+    // Max size of 18 at this point because 2^18 is massive and is larger than the array size limit. Going higher is
+    // maybe possible if we use std::vector or std::array instead, but forcing anything to have >2^18 rows might be
+    // challenging no matter what.
+    const int size = 18;
+    unsigned int rows = std::pow(2, size);
 
-std::vector<std::vector<int>> genCombinations(std::array<int, size> comb, int i) {
-    std::vector<std::vector<int>> total;
-    if(i < size)
-    {
-        comb[i] = 0;
-        std::vector<std::vector<int>> temp1 = genCombinations(comb, i + 1);
-
-        total.insert(
-                total.end(),
-                std::make_move_iterator(temp1.begin()),
-                std::make_move_iterator(temp1.end())
-        );
-
-        comb[i] = 1;
-        std::vector<std::vector<int>> temp2 = genCombinations(comb, i + 1);
-
-        total.insert(
-                total.end(),
-                std::make_move_iterator(temp2.begin()),
-                std::make_move_iterator(temp2.end())
-        );
+    // This generates all combinations of k bit-strings. Each row i is a unique set of combinations
+    bool combinations[rows][size];
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < size; j++) {
+            int val = rows * j + i;
+            int ret = (1 & (val >> j));
+            combinations[i][j] = ret != 0;
+        }
     }
 
-    return total;
+    // This is a rough example of how the combination array can be used to generate a second array using the combination
+    // values. In order to not use tons of ram, probably we would want to create two sets of references per generator:
+    // one in an off state and one in an on state and load our generatorlist with references rather than unique objects
+    // in memory.
+    char test[size];
+    for(int i = 0; i < size; i++) {
+        if(combinations[5][i] == 0) {
+            test[i] = 'n';
+        } else {
+            test[i] = 'y';
+        }
+    }
+
+    // Print out the test array just to see that the different characters are assigned based on the 0/1 combo values.
+    for(char c : test) {
+        std::cout << c << '\t';
+    }
 }
