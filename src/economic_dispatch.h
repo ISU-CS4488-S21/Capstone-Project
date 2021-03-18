059489396452e80896f4a1dd61b76616323aec05
Nate-Shubert <70928296+Nate-Shubert@users.noreply.github.com>
2021-03-18 07:05:12 -0600

    Create economic_dispatch.h

---
+++ src/economic_dispatch.h
@@ -0,0 +1,20 @@
+//
+// Created by Nate Shubert on 3/17/21.
+//
+#include <string>
+#include <vector>
+#include <generator.h>
+
+#ifndef UNIT_COMMITMENT_ECONOMIC_DISPATCH_H
+#define UNIT_COMMITMENT_ECONOMIC_DISPATCH_H
+
+class Economic_Dispatch{
+private:
+
+public:
+    Economic_Dispatch();
+    double lambdaFunction(double load, const std::vector<Generator>& generators, int index);
+    std::vector<std::vector<Generator>>& onGenerators(std::vector<std::vector<Generator>>& generators);
+};
+
+#endif //UNIT_COMMITMENT_ECONOMIC_DISPATCH_H
