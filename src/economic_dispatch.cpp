1e7d9b8bc74ed4a45874605ee5c06c6f486f1f02
Nate-Shubert <70928296+Nate-Shubert@users.noreply.github.com>
2021-04-30 11:34:27 -0700

    Update economic_dispatch.cpp

--- src/economic_dispatch.cpp
+++ src/economic_dispatch.cpp
@@ -11,7 +11,7 @@
 Economic_Dispatch::Economic_Dispatch(){
     skippedLoads = 0;
 }
-
+// A function that removes the Off Generators from a combination, and leaves only the on ones.
 std::vector<std::vector<Generator>> & Economic_Dispatch::onGenerators(std::vector<std::vector<Generator>> &generators) {
     // Narrow down to only on generators
     std::vector<std::vector<Generator>> onGenCombos;
@@ -29,7 +29,8 @@ std::vector<std::vector<Generator>> & Economic_Dispatch::onGenerators(std::vecto
     }
     return onGenCombos;
 }
-
+// Divide provides a structure to call the Lambda function. It uses the onGenerators function to narrow
+// down to the on generators. The major benefit of this is that is divides the combination in half to provide faster computation.
 double Economic_Dispatch::divide(double load, std::vector<Generator> &generators) {
     std::vector<Generator> onGens;
     for(const auto& combo : generators){
@@ -51,6 +52,7 @@ double Economic_Dispatch::divide(double load, std::vector<Generator> &generators
        // }
     }
     else{
+// if the size of vectors is larger than 1, divide in half and perform "merge"
         int half = trunc(onGens.size()/2);
         std::vector<Generator> firstHalf;
         std::vector<Generator> secondHalf;
@@ -63,15 +65,19 @@ double Economic_Dispatch::divide(double load, std::vector<Generator> &generators
                 secondHalf.push_back(onGens[i]);
             }
         }
+// call Merge function to get lambda cost
         out = merge(load,firstHalf,secondHalf);
     }
     return out;
 }
 
+// this function takes two combinations of vectors, calculates the cheapest operating cost.
 double Economic_Dispatch::merge(double load, std::vector<Generator> &g1, std::vector<Generator> &g2) {
     double mLoad = load;
     double out = std::numeric_limits<int>::max();
+// a while loop is used to check the cost at all available loads. The costs are compared and the cheapest is selected.
     while(load > 0){
+// use check bound to not calculate any loads out of range.
         if (checkBound(load, const_cast<std::vector<Generator> &>(g1)) && checkBound(load, const_cast<std::vector<Generator> &>(g2))){
             double one = lambdaFunction(load,g1,g1.size());
             double two = lambdaFunction(mLoad - load,g2,g2.size());
@@ -110,6 +116,8 @@ bool Economic_Dispatch::checkBound(double load, std::vector<Generator> &g1) {
 double Economic_Dispatch::lambdaFunction(double load, const std::vector<Generator>& generators, int index) {
     std::vector<std::pair<double,double>> gen;
     double out;
+// create a pair out of an array of generators to hold B and C values. 
+// this was done very early and may be an easy change for speed.
     for(auto elem : generators){
             gen.push_back(std::make_pair<double, double>(elem.getB(), elem.getC()));
     }
@@ -117,9 +125,11 @@ double Economic_Dispatch::lambdaFunction(double load, const std::vector<Generato
     double temp;
     double min = std::numeric_limits<int>::max();
     double g2,g1;
+// if an array of 1 generator then just return the ED of that gen
     if(index == 1){
         return gen[0].first*load + gen[0].second*pow(load,2);
     }
+// compare two generators at varying loads to get cheapest option between them.
     if(index < 1){
         //The final branch of the generator tree to return optimized cost.
         while(load > 0){
@@ -132,6 +142,8 @@ double Economic_Dispatch::lambdaFunction(double load, const std::vector<Generato
             load -= 1;
         }
     }
+// the recursive part of the function. Compare the outermost generator with a combination of the rest.
+// the recursion continues until there is just one pair of generators to find the cost.
     else{
         // Determine the cost at every possible load value.
         // Recursively cal the function to branch off every cost possibility
